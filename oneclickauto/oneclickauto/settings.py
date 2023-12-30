a11ac23c03529a58ebf0e8d8f5d3aa08fffcc4d9
danny0311 <126737149+thanhnguyen0311@users.noreply.github.com>
2023-12-30 07:10:12 +0700

    Update settings.py

--- oneclickauto/oneclickauto/settings.py
+++ oneclickauto/oneclickauto/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^k(x!uymgcrwt!^(br@31a47c+1mnl)mvtc2fq)fb)1gyp#+gv'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
